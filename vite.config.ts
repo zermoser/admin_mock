7df23e72b56be63990353045870ef169bfc2bab1
zermoser <kittipoj.na@gmail.com>
2025-06-18 00:42:12 +0700

    feat: add new index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/admin_mock/',
   server: {
-    port: 3000
+    open: true,
+    port: 3007
   }
 });
